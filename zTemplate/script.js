bedc4059cf792546e48609b6d945326f4255d53b
taiseen <taiseen.cse@gmail.com>
2023-09-19 00:20:44 +0600

    refactor: canvas template

--- zTemplate/script.js
+++ zTemplate/script.js
@@ -1,25 +1,76 @@
-// 7 - Sep - 2023
+// 07 - Sep - 2023 - setup normal template...
+// 18 - Sep - 2023 - class base refactor...
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+class Canvas {
+    constructor() {
+        this.canvas = document.getElementById('canvas');
+        this.context = this.canvas.getContext('2d');
 
-const cWidth = canvas.width = window.innerWidth;
-const cHeight = canvas.height = window.innerHeight;
+        this.cWidth = this.canvas.width = 900;
+        this.cHeight = this.canvas.height = 600;
 
+        this.startAnimation();
+    }
 
-const rectangleSize = 100;
+    drawCenterRect(context) {
+        const rS = 100;
+        const cW = this.cWidth;
+        const cH = this.cHeight;
 
-ctx.fillStyle = 'tomato'
-ctx.fillRect(100, 100, rectangleSize, rectangleSize);
-ctx.fillRect(200, 200, rectangleSize, rectangleSize);
+        // =======================================================================
 
+        // center...
+        context.fillStyle = 'pink';
+        context.fillRect((cW - rS) / 2, (cH - rS) / 2, rS, rS);
 
-const startAnimation = () => {
-    ctx.clearRect(0, 0, cWidth, cHeight); // clear all previous frames...
+        context.fillStyle = 'orange';
+        context.fillRect((cW - rS) / 2 - 100, (cH - rS) / 2, rS, rS);
+        context.fillStyle = 'orange';
+        context.fillRect((cW - rS) / 2 + 100, (cH - rS) / 2, rS, rS);
 
-    ctx.fillStyle = 'tomato'
-    ctx.fillRect(500, 500, rectangleSize, rectangleSize);
+        context.fillStyle = 'tomato';
+        context.fillRect((cW - rS) / 2, (cH - rS) / 2 - 100, rS, rS);
+        context.fillStyle = 'tomato';
+        context.fillRect((cW - rS) / 2, (cH - rS) / 2 + 100, rS, rS);
 
-    requestAnimationFrame(startAnimation);
+        // =======================================================================
+
+        // corner area...
+        context.fillStyle = 'lightgreen';
+        context.fillRect(0, 0, rS, rS);
+
+        context.fillStyle = 'lightgreen';
+        context.fillRect(cW - rS, 0, rS, rS);
+
+        context.fillStyle = 'lightgreen';
+        context.fillRect(0, cH - rS, rS, rS);
+
+        context.fillStyle = 'lightgreen';
+        context.fillRect(cW - rS, cH - rS, rS, rS);
+
+        // =======================================================================
+
+        // middle area...
+        context.fillStyle = 'pink';
+        context.fillRect((cW - rS) / 2, 0, rS, rS);
+
+        context.fillStyle = 'pink';
+        context.fillRect((cW - rS) / 2, cH - rS, rS, rS);
+
+        context.fillStyle = 'pink';
+        context.fillRect(0, (cH - rS) / 2, rS, rS);
+
+        context.fillStyle = 'pink';
+        context.fillRect(cW - rS, (cH - rS) / 2, rS, rS);
+    }
+
+    startAnimation = () => {
+        this.context.clearRect(0, 0, this.cWidth, this.cHeight); // clear all previous frames...
+
+        this.drawCenterRect(this.context);
+
+        requestAnimationFrame(this.startAnimation);
+    }
 }
-// startAnimation();
+
+window.addEventListener('load', () => new Canvas());
