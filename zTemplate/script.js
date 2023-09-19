d23bf6fc6b9e2302b1ddb4970f197fd2a611c14f
taiseen <taiseen.cse@gmail.com>
2023-09-20 00:49:31 +0600

    feat: at template drawLine add

--- zTemplate/script.js
+++ zTemplate/script.js
@@ -64,10 +64,72 @@ class Canvas {
         context.fillRect(cW - rS, (cH - rS) / 2, rS, rS);
     }
 
+    drawLine(context) {
+        const cW = this.cWidth;
+        const cH = this.cHeight;
+
+        // diagonal line...
+        context.strokeStyle = 'red'
+        context.beginPath();
+        context.moveTo(0, 0);
+        context.lineTo(cW, cH);
+        context.stroke();
+
+        // opposite diagonal line...
+        context.strokeStyle = 'orange'
+        context.beginPath();
+        context.moveTo(cW, 0);
+        context.lineTo(0, cH);
+        context.stroke();
+
+        // center vertical line...
+        context.strokeStyle = 'green'
+        context.beginPath();
+        context.moveTo(cW / 2 + 60, 0);
+        context.lineTo(cW / 2 + 60, cH);
+        context.stroke();
+
+        // center horizontal line...
+        context.strokeStyle = 'green'
+        context.beginPath();
+        context.moveTo(0, cH / 2 + 60);
+        context.lineTo(cW, cH / 2 + 60);
+        context.stroke();
+
+        // half left ==> horizontal line...
+        context.strokeStyle = 'black'
+        context.beginPath();
+        context.moveTo(0, cH / 2);
+        context.lineTo(cW / 2, cH / 2);
+        context.stroke();
+
+        // half right ==> horizontal line...
+        context.strokeStyle = 'gray'
+        context.beginPath();
+        context.moveTo(cW / 2, cH / 2);
+        context.lineTo(cW, cH / 2);
+        context.stroke();
+
+        // half bottom ==> vertical line...
+        context.strokeStyle = 'blue'
+        context.beginPath();
+        context.moveTo(cW / 2, 0);
+        context.lineTo(cW / 2, cH / 2);
+        context.stroke();
+
+        // half top ==> vertical line...
+        context.strokeStyle = 'brown'
+        context.beginPath();
+        context.moveTo(cW / 2, cH / 2);
+        context.lineTo(cW / 2, cH);
+        context.stroke();
+    }
+
     startAnimation = () => {
         this.context.clearRect(0, 0, this.cWidth, this.cHeight); // clear all previous frames...
 
         this.drawCenterRect(this.context);
+        this.drawLine(this.context);
 
         requestAnimationFrame(this.startAnimation);
     }
