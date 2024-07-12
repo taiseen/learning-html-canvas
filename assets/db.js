0dec3e1c43a25cde6795af8bf68999df9a856d4a
taiseen <taiseen.cse@gmail.com>
2024-07-13 00:57:43 +0600

    fix: broken link for opening projects

--- assets/db.js
+++ assets/db.js
@@ -1,51 +1,51 @@
 const projects = [
     {
         name: 'Basic Flower Shape',
-        link: '../01.Basic-FlowerShape/index.html',
+        link: './01.Basic-FlowerShape/index.html',
     },
     {
         name: 'Basic Canvas',
-        link: '../Basic-Canvas/index.html',
+        link: './Basic-Canvas/index.html',
     },
     {
         name: 'Games | Bubble',
-        link: '../Games/Bubble/index.html',
+        link: './Games/Bubble/index.html',
     },
     {
         name: 'Games | Ping-Pong',
-        link: '../Games/Ping-Pong/index.html',
+        link: './Games/Ping-Pong/index.html',
     },
     {
         name: 'Games | Seahorse',
-        link: '../Games/Seahorse/index.html',
+        link: './Games/Seahorse/index.html',
     },
     {
         name: 'Games | Snake-GreenBg',
-        link: '../Games/Snake-GreenBg/index.html',
+        link: './Games/Snake-GreenBg/index.html',
     },
     {
         name: 'Key Movement Control',
-        link: '../Key-Movement-Control/index.html',
+        link: './Key-Movement-Control/index.html',
     },
     {
         name: 'Map Canvas',
-        link: '../Map-Canvas/index.html',
+        link: './Map-Canvas/index.html',
     },
     {
         name: 'Mouse Move Pixels Physics',
-        link: '../Mouse-Move-Pixels-Physics/index.html',
+        link: './Mouse-Move-Pixels-Physics/index.html',
     },
     {
         name: 'Read Update Img Pixel', 
-        link: '../Read-Update-Img-Pixel/index.html',
+        link: './Read-Update-Img-Pixel/index.html',
     },
     {
         name: 'zHtml',
-        link: '../zHtml/index.html',
+        link: './zHtml/index.html',
     },
     {
         name: 'zTemplate',
-        link: '../zTemplate/index.html',
+        link: './zTemplate/index.html',
     },
     
 ]
--- assets/script.js
+++ assets/script.js
@@ -2,19 +2,32 @@ import projects from './db.js';
 
 const container = document.getElementById('container');
 
+const gitUrl = 'https://taiseen.github.io/learning-html-canvas/';
+
+
 // Loop through each project and create an anchor element
 projects.forEach(project => {
+
     // Create the anchor element
     const anchor = document.createElement('a');
 
-    // Set the required attributes
-    anchor.setAttribute('href', project.link);
+
+    // 🔶 Set the required attributes || for local live Link
+    // anchor.setAttribute('href', project.link);
+
+
+    // 🔶 Set the required attributes || for gitHub live Link
+    anchor.setAttribute('href', `${gitUrl}${project.link}`);
+
+
     // anchor.setAttribute('rel', 'noopener noreferrer');
     anchor.setAttribute('target', '_blank');
+
 
     // Set the inner text of the anchor
     anchor.textContent = project.name;
 
+    
     // Append the anchor to the container
     container.appendChild(anchor);
 });
